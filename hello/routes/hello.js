ae42b309fc406abe3c47062c033c0abde0f583c6
Valentina Carreno <v1carren@ucsd.edu>
2021-01-21 11:34:23 -0800

    lab3 update

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,12 @@
  */
 
 exports.view = function(req, res){
+  var nameToShow = req.params.userName;
+  if (nameToShow === undefined) {
+  	nameToShow = 'Valentina';
+  }
+console.log("Name is " + nameToShow);
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -4,15 +4,20 @@
 $(document).ready(function() {
 	initializePage();
 })
-
+function projectClick(e) {
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
+}
 /*
  * Function that is called when the document is ready.
  */
+
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
-
+	
+	$("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
